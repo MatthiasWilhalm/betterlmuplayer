4e4fc35959e2dcef723ea51980a1c20e6920b9ca
MatthiasWilhalm <matthias.wilhalm@gmail.com>
2021-04-15 11:49:38 +0200

    improve code quality

--- script.js
+++ script.js
@@ -5,69 +5,77 @@
 // ==/UserScript==
 
 var timeout;
-      
+
 function hideMeunBar() {
-console.log("hide");
+  console.log("hide");
   document.getElementById("player-control-panel").classList.remove("player-control-panel-show");
   document.getElementById("player-control-panel").classList.add("player-control-panel-hover");
 }
 
 function unhideMenuBar() {
   console.log("unhide");
-     document.getElementById("player-control-panel").classList.remove("player-control-panel-hover");
+  document.getElementById("player-control-panel").classList.remove("player-control-panel-hover");
   document.getElementById("player-control-panel").classList.add("player-control-panel-show");
 }
 
 function loadHoverIcon(mode, msg, showCenter) {
-if(mode!==undefined) {
-let vid = document.getElementById("player");
-  let pla = document.getElementById("view-box1");
-  console.log(vid);
-var ic = document.createElement("div");
-ic.classList.add("player-hover-icon");
-  ic.classList.add("hover-"+mode);
-let te = document.createElement("p");
-if(msg!==undefined) {
-    te.innerHTML = msg;
-    te.classList.add(showCenter?"player-hover-text-center":"player-hover-text");
-    ic.appendChild(te);
-}
-vid.insertBefore(ic, pla);
-setTimeout(function(){
-  ic.remove();
-}, 1000);
-}
+  if (mode !== undefined) {
+    let vid = document.getElementById("player");
+    let pla = document.getElementById("view-box1");
+    //console.log(vid);
+    var ic = document.createElement("div");
+    ic.classList.add("player-hover-icon");
+    ic.classList.add("hover-" + mode);
+    let te = document.createElement("p");
+    if (msg !== undefined) {
+      te.innerHTML = msg;
+      te.classList.add(showCenter ? "player-hover-text-center" : "player-hover-text");
+      ic.appendChild(te);
+    }
+    vid.insertBefore(ic, pla);
+    setTimeout(function () {
+      ic.remove();
+    }, 1000);
+  }
 };
 
-setTimeout(function(){
-   loadHoverIcon();
-//unhideMeunBar();
+setTimeout(function () {
+  loadHoverIcon();
+  //unhideMeunBar();
 }, 3000);
 document.onload = loadHoverIcon();
 
 document.getElementById("player").addEventListener('click', e => {
-if(e.target.id==="") {
-playPause();
-}
+  if (e.target.id === "") {
+    playPause();
+  }
 });
 
 document.addEventListener('keydown', k => {
-let key = k.keyCode;
-if(k.shiftKey && key===190) {
+  let key = k.key;
+  if (k.shiftKey && key === ':') {
     faster();
-} else if (k.shiftKey && key===188) {
+  } else if (k.shiftKey && key === ';') {
     slower();
-} else if(key === 76) {
-        skip();
-   } else if(key === 74) {
-    back();      
-}
+  } else if (key === 'l') {
+    skip();
+  } else if (key === 'j') {
+    back();
+  } else if(key === 'ArrowRight') {
+    skip(5);
+  } else if(key === 'ArrowLeft') {
+    back(5);
+  } else if(key === 'L') {
+    skip(60);
+  } else if(key === 'J') {
+    back(60);
+  }
 });
 
 document.addEventListener('keyup', k => {
-  if(k.keyCode===32 || k.keyCode===75) {
+  if (k.key === ' ' || k.key === 'k') {
     playPause();
-}
+  }
 });
 /*
 document.addEventListener("mousemove", function() {
@@ -87,39 +95,41 @@ function getVid() {
 
 function playPause() {
   let vid = getVid();
-      if(vid.paused) {
+  if (vid.paused) {
     loadHoverIcon("Play");
     vid.play();
-    } else {
+  } else {
     loadHoverIcon("Pause");
     vid.pause();
-    }
+  }
 }
 
 function faster() {
-    let vid = getVid();
-    if(vid.playbackRate<2.0) {
-      vid.playbackRate += 0.25;
-      loadHoverIcon("speed_up", vid.playbackRate+"x");
-}
+  let vid = getVid();
+  if (vid.playbackRate < 2.0) {
+    vid.playbackRate += 0.25;
+    loadHoverIcon("speed_up", vid.playbackRate + "x");
+  }
 }
 
 function slower() {
-      let vid = getVid();
-    if(vid.playbackRate>0.25) {
-      vid.playbackRate -= 0.25;
-      loadHoverIcon("slow_down", vid.playbackRate+"x");
-}
-}
-
-function skip() {
-    let vid = getVid();
-    vid.currentTime+=10;
-    loadHoverIcon("skip", 10, true);
+  let vid = getVid();
+  if (vid.playbackRate > 0.25) {
+    vid.playbackRate -= 0.25;
+    loadHoverIcon("slow_down", vid.playbackRate + "x");
+  }
 }
 
-function back() {
-    let vid = getVid();
-vid.currentTime-=10;
-loadHoverIcon("rev", 10, true);
+function skip(time) {
+  let st = time || 10;
+  let vid = getVid();
+  vid.currentTime += st;
+  loadHoverIcon("skip", st, true);
+}
+
+function back(time) {
+  let st = time || 10;
+  let vid = getVid();
+  vid.currentTime -= st;
+  loadHoverIcon("rev", st, true);
 }
